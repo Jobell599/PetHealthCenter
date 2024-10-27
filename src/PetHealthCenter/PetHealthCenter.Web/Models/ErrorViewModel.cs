8aae4dfd0d7d8fff792beade7aeef34511f82e98
Jobell599 <jobeldicley123@gmail.com>
2024-10-27 19:54:15 -0400

    initial commit

---
+++ src/PetHealthCenter/PetHealthCenter.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetHealthCenter.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
