1868c45c284b2551b1bd84b734e80f39274808be
Jobell599 <jobeldicley123@gmail.com>
2024-10-28 23:31:09 -0400

    Complete initial architecture

---
+++ src/PetHealthCenter/PetHealthCenter.Webb/Data/PetHealthCenterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetHealthCenter.Domain.Entities;
+
+namespace PetHealthCenter.Webb.Data
+{
+    public class PetHealthCenterDbContext : DbContext
+    {
+        public PetHealthCenterDbContext (DbContextOptions<PetHealthCenterDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PetHealthCenter.Domain.Entities.Clients> Clients { get; set; } = default!;
+    }
+}
--- src/PetHealthCenter/PetHealthCenter.Webb/Models/Entities/Clients.cs
+++ src/PetHealthCenter/PetHealthCenter.Webb/Models/Entities/Clients.cs
@@ -1,6 +1,18 @@
-﻿namespace PetHealthCenter.Domain.Entities
+﻿using Microsoft.IdentityModel.Protocols.Configuration;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetHealthCenter.Domain.Entities
 {
     public class Clients
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName {  get; set; }
+        public string Address {  get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
     }
 }
